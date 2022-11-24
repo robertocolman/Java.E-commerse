33476259f85010ceb5945e0a96935cd384587b98
robertocolman <robertocolman93@gmail.com>
2022-11-23 21:12:31 -0300

    se etrega

---
+++ js/products.js
@@ -0,0 +1,34 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Cafe suave",
+    precio: 1400,
+    img:
+      "https://www.repartienda.com/wp-content/uploads/2020/04/IMG_6904.jpg",
+    cantidad: 30,
+  },
+  {
+    id: 2,
+    nombre: "Cafe gold",
+    precio: 2000,
+    img:
+      "https://http2.mlstatic.com/D_NQ_NP_743276-MLA46625047062_072021-V.jpg",
+    cantidad: 45,
+  },
+  {
+    id: 3,
+    nombre: "Capuccino",
+    precio: 1900,
+    img:
+      "https://www.nescafe.com/ar/sites/default/files/2022-06/Mixes_Png-04.png",
+    cantidad: 2,
+  },
+  {
+    id: 4,
+    nombre: "cafe molido ",
+    precio: 2500,
+    img:
+      "https://www.nescafe.com/ar/sites/default/files/2021-11/GOLD%20INTENSO%20N9.png",
+    cantidad: 1,
+  },
+];
